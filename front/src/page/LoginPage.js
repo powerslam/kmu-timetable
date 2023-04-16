43642b1095b89c143bc59f79cb3c86371892ef36
timsort <timsort@kookmin.ac.kr>
2023-04-16 19:00:39 +0900

    너비 대응응

--- front/src/page/LoginPage.js
+++ front/src/page/LoginPage.js
@@ -3,19 +3,19 @@ import loginStyles from "../styles/LoginPage.module.css";
 import signBtnStyles from "../styles/SignButton.module.css";
 
 import userIcon from './user.svg';
-import pwdImg from './pwd.png';
+import pwdIcon from './pwd.svg';
 
 const LoginPage = () => {
     return (
         <div className={loginStyles.container}>
             <h1>로그인</h1>
             <div className={loginStyles.textfield}>
-                <img style={{ marginRight: "10px", width: "75%", height: "75%"}} src={userIcon} alt="아이디" />
+                <img className={loginStyles.icon} src={userIcon} alt="아이디" />
                 <TextField type="text" placeholder="아이디" />
             </div>
 
             <div className={loginStyles.textfield}>
-                <img style={{ marginRight: "10px", width: "75%", height: "75%"}} src={pwdImg} alt="비밀번호" />
+                <img className={loginStyles.icon} src={pwdIcon} alt="비밀번호" />
                 <TextField type="password" placeholder="비밀번호" />
             </div>
 
