3ea126e3b72f20f4c86ec46712328affa545ab7d
timsort <timsort@kookmin.ac.kr>
2023-04-28 13:55:27 +0900

    디자인 코드 수정

--- front/src/components/common/TextField.js
+++ front/src/components/common/TextField.js
@@ -1,7 +1,12 @@
 import styles from '../../styles/Textfield.module.css';
 
-const TextField = ({type, placeholder}) => {
-    return <input className={styles.textfield} type={type} placeholder={placeholder} />
+const TextField = ({id, type, placeholder, onChange, value, addClassName=''}) => {
+    return <input id={id} 
+        className={`${styles.textfield} ${addClassName}`} 
+        type={type} 
+        placeholder={placeholder} 
+        onChange={onChange}
+        value={value} />
 }
 
 export default TextField;
