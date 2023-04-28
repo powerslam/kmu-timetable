afc7c431bcb24859e89a1e2ac796ae83d8f3b844
timsort <timsort@kookmin.ac.kr>
2023-04-28 13:54:57 +0900

    서버에 로그인 요청 후 계정 유무 / 틀림 반환 구현

--- front/src/page/LoginPage.js
+++ front/src/page/LoginPage.js
@@ -1,29 +1,76 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import TextField from "../components/common/TextField";
 import loginStyles from "../styles/LoginPage.module.css";
 import signBtnStyles from "../styles/SignButton.module.css";
 
-import userIcon from './user.svg';
-import pwdIcon from './pwd.svg';
+import userIcon from '../resources/user.svg';
+import pwdIcon from '../resources/pwd.svg';
+
+import { useNavigate } from "react-router-dom";
+
+import { SIGNUP, MAIN } from '../lib/variables';
 
 const LoginPage = () => {
+    const [ email, setEmail ] = useState("");
+    const [ pwd, setPwd ] = useState("");
+    const navigate = useNavigate();
+
+    const onChange = (e) => {
+        switch(e.target.id){
+            case "email": 
+                setEmail(e.target.value);
+                break;
+            
+            case "pwd": 
+                setPwd(e.target.value);
+                break;
+        }
+    }
+
+    const login = (e) => {
+        e.preventDefault();
+
+        if(!email) alert("이메일이 비었습니다.");
+        else if(!pwd) alert("비밀번호가 비었습니다.");
+        else{
+            axios.defaults.headers['Access-Control-Allow-Origin'] = '*';
+            axios.post("https://kmu-timtable-ivort.run.goorm.site/login", {
+                id: email,
+                pwd: pwd,
+            }).then((res) => {
+                if(res.data === 'fail') {
+                    alert('아이디가 존재하지 않거나 비밀번호가 틀렸습니다.');
+                } else {
+                    alert('와 로그인 성공!');
+                    navigate(MAIN);
+                }
+
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }
+
     return (
-        <div className={loginStyles.container}>
+        <form className={loginStyles.container} onSubmit={login}>
             <h1>로그인</h1>
             <div className={loginStyles.textfield}>
                 <img className={loginStyles.icon} src={userIcon} alt="아이디" />
-                <TextField type="text" placeholder="아이디" />
+                <TextField id="email" type="text" placeholder="아이디" onChange={onChange} value={email} />
             </div>
 
             <div className={loginStyles.textfield}>
                 <img className={loginStyles.icon} src={pwdIcon} alt="비밀번호" />
-                <TextField type="password" placeholder="비밀번호" />
+                <TextField id="pwd" type="password" placeholder="비밀번호" onChange={onChange} value={pwd} />
             </div>
 
             <div className={loginStyles.btnfield}>
                 <button className={signBtnStyles.sign}>로그인</button>
-                <button className={signBtnStyles.sign}>회원가입</button>
+                <button className={signBtnStyles.sign} onClick={() => navigate(SIGNUP)}>회원가입</button>
             </div>
-        </div>
+        </form>
     );
 };
 
