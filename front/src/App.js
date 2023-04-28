ecbf667234b837993503e5f3274de450eeec11e9
timsort <timsort@kookmin.ac.kr>
2023-04-28 13:57:52 +0900

    라우팅 코드 추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,33 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { MAIN, LOGIN, SIGNUP } from './lib/variables';
+
+import MainPage from './page/MainPage';
+import LoginPage from './page/LoginPage';
+import SignUpPage from './page/SignUpPage';
+
 import { TimeTableProvider } from './lib/TimeTableContext';
 
 import TimeTable from './components/TimeTable/TimeTable';
-import AddButton from './components/SearchMenu/AddButton';
 import SearchMenu from './components/SearchMenu/SearchMenu';
 
 function App() {
   return (
-    <div style={{
-      textAlign: "center",
-    }}>
-      <TimeTableProvider>
-        <TimeTable />
-        <AddButton />
-        <SearchMenu />
-      </TimeTableProvider>
-    </div>  
+    <Routes>
+      <Route exact path={ MAIN } element={<MainPage />}/>
+      <Route exact path={ LOGIN } element={<LoginPage/>} />
+      <Route exact path={ SIGNUP } element={<SignUpPage />} />
+      <Route exact path="/timetable" element={
+        <div style={{
+          textAlign: "center",
+        }}>
+          <TimeTableProvider>
+            <TimeTable />
+            <SearchMenu />
+          </TimeTableProvider>
+        </div>  
+      }/>
+    </Routes>
   );
 }
 
