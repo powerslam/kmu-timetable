a64f7f8ff0ae5142725da469e5177384aa87b59e
timsort <timsort@kookmin.ac.kr>
2023-04-30 18:47:14 +0900

    CardList 구현

---
+++ front/src/components/common/CardList.js
@@ -0,0 +1,18 @@
+import "../../styles/CardList.css";
+import { weekly, NULL_STR } from "../../lib/variables";
+
+import StackCard from "./Card";
+
+const CardList = () => {
+    return (
+        <div className="CardList">{
+            weekly[0].daytime.time.map((v, i) => {
+                console.log(v);
+                if(v === NULL_STR) return null;
+                return <Card key={i} sbj_nm={v}/>
+            })
+        }</div>
+    )
+}
+
+export default CardList;
