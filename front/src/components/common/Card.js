18433320d8667369022b4fc0d3ce1cc7bc12aae3
powerslam <timsort@kookmin.ac.kr>
2023-05-02 03:10:46 +0900

    각 페이지의 특성에 맞게 Card를 구성하기 위해 List 뷰는 제거

--- front/src/components/common/Card.js
+++ front/src/components/common/Card.js
@@ -1,14 +1,10 @@
-import "../../styles/CardList.css";
+import "../../styles/Card.css";
 
-const Card = ({ sbj_nm }) => {
+const Card = ({ children }) => {
     return (
         <div className="Card">
-            <div className="font-bold text-3xl mb-2 px-6 py-4 max-sm:text-xl max-lg:text-2xl">{sbj_nm}</div>
-            <div className="flex flex-row items-center justify-center max-md:flex-col">
-                <span className="bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 max-md:mb-2">남은 시간</span>
-                <span className="mx-2">남았습니다.</span>
-            </div>
-        </div>  
+            { children }
+        </div>
     );
 };
 
