9adb7126ca99993db8f28fdc0e6a5acff81015af
timsort <timsort@kookmin.ac.kr>
2023-05-01 21:53:30 +0900

    축소 시 텍스트 -> 아이콘

--- front/src/components/NavBar.js
+++ front/src/components/NavBar.js
@@ -1,29 +1,41 @@
 import { NavLink } from "react-router-dom";
 import { MAIN, LOGIN, TIMETABLE } from "../lib/variables";
 
+import { ReactComponent as BusIcon } from "../resources/bus.svg";
+import { ReactComponent as EmptyRoomIcon } from "../resources/emptyroom.svg";
+import { ReactComponent as ScheduleIcon } from "../resources/schedule.svg";
+
+import "../styles/NavBar.css";
+
 const NavBar = () => {
     return (
-        <nav className="flex flex-row items-center justify-between bg-teal-500 w-full h-[8%] ">
+        <nav className="menu-body">
             <div className="flex flex-shrink-0 text-white mx-6 max-md:mx-4">
                 <NavLink to={ MAIN }>
                     <span className="font-semibold tracking-tight text-5xl max-md:text-3xl max-[380px]:text-2xl">K-Time</span>
                 </NavLink>
             </div>
 
-            <div className="flex flex-row items-center max-md:hidden">
-                <NavLink to={ LOGIN }>
-                    <span className="text-teal-200 hover:text-white text-2xl mr-6">로그인</span>
-                </NavLink>
-                
+            <div className="flex flex-row items-center">
                 <NavLink to={ TIMETABLE }>
-                    <span className="text-teal-200 hover:text-white text-2xl mr-6">시간표</span>
+                    <span className="menu-text">시간표</span>
+                    <ScheduleIcon className="menu-icon"/>
                 </NavLink>
-            </div>
 
-            <div className="md:hidden mr-6">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
+                <NavLink to={ TIMETABLE }>
+                    <span className="menu-text">빈 강의실</span>
+                    <EmptyRoomIcon className="menu-icon"/>
+                </NavLink>
+
+                <NavLink to={ TIMETABLE }>
+                    <span className="menu-text">교통정보</span>
+                    <BusIcon className="menu-icon"/>
+                </NavLink>
+
+                <NavLink to={ LOGIN }>
+                    <span className="menu-text">로그인</span>
+                    <BusIcon className="menu-icon"/>
+                </NavLink>
             </div>
         </nav>
     )
