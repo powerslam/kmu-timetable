1c58e73879dfdefcd5d21fe4e401f7a794da5977
timsort <timsort@kookmin.ac.kr>
2023-05-05 03:12:44 +0900

    API 받아와서 강의실 별 금일 시간표 노출

--- front/src/page/EmptyClassRoomPage.js
+++ front/src/page/EmptyClassRoomPage.js
@@ -24,12 +24,24 @@ const EmptyClassRoomPage = () => {
     }, []);
 
     const getFloorList = (buildingCD) => {
+        // 건물을 선택하면서 해당 건물의 층 수를 가져온다.
+        // 그래서 층 수 선택자를 초기화 하기 위해 selectedIndex 를 0으로 하고
+        // 층 수가 모두 로드되기 전엔 true로 준다.
+        floorSelector.current.selectedIndex = 0;
+        floorSelector.current.disabled = true;
+
+        // 건물이 달라졌기 때문에 null로 바꾼다
+        setClassRoomList(null);
+
         const getData = async () => {
             const data = await axios.get(`https://kmu-timtable-ivort.run.goorm.site/building/${buildingCD}`)
                 .then(res => res.data)
                 .then(data => data.map(({ FLOOR }) => FLOOR))
                 .catch(err => console.log(err));
             setFloorList(data);
+
+            // 층 수가 모두 로드되면 disabled를 푼다.
+            floorSelector.current.disabled = false;
         }
         
         getData();
@@ -46,7 +58,7 @@ const EmptyClassRoomPage = () => {
                         if(!rooms.hasOwnProperty(CLASSROOM_NM)) rooms[CLASSROOM_NM] = [];
                         rooms[CLASSROOM_NM].push(classInfo);
                     });
-
+                    console.log(rooms);
                     return rooms;
                 })
                 .catch(err => console.log(err));
@@ -57,27 +69,26 @@ const EmptyClassRoomPage = () => {
     };
 
     const onBuildingChange = (e) => {
-        // 기본 선택지를 고르면
+        // 기본 선택지(건물을 선택하세요)를 고르면
         if(e.target.value === "-11") {
+            // 층 수 선택자를 초기화
             floorSelector.current.selectedIndex = 0;
             floorSelector.current.disabled = true;
             setFloorList(null);
+
+            // 아래 화면도 초기화
             setClassRoomList(null);
         } 
         
-        // 건물을 골랐을 경우
-        else {
-            floorSelector.current.selectedIndex = 0;
-            floorSelector.current.disabled = false;
-            getFloorList(e.target.value);
-        }
+        // 건물을 골랐을 경우, FloorList를 가져옴
+        else getFloorList(e.target.value);
     };
 
     const onFloorChange = (e) => {
-        // 기본 선택지를 고르면
+        // 기본 선택지를 고르면 아래 화면을 초기화 한다
         if(e.target.value === "-11") setClassRoomList(null);
         
-        // 층을 골랐을 경우
+        // 층을 골랐을 경우 그 층에 맞는 강의실 리스트를 가져온다
         else getClassRoomList(buildingSelector.current.selectedIndex, e.target.value);
     };
 
@@ -98,28 +109,22 @@ const EmptyClassRoomPage = () => {
             </select>
         </div>
         {classRoomList && Object.keys(classRoomList).map(classRoom => {
-            return (
-                <Card key={classRoom}>
-                    <div className="Card-Title">{classRoom}</div>
-                    <div className="Card-Detail">
-                        {
-                            classRoomList[classRoom].map(info => (
-                                <div className="flex flex-row justify-items-center my-2">
-                                    <span className="mx-2">{info.SUBJECT_NM}</span>
-                                    <span className="mx-2">{parseInt(info.START.match(/\d+/)[0]) + 8}:{(info.START.split(/\d+/)[1] === 'B' ? '30' : '00')}</span>
-                                    <span className="mx-2">{info.TIME}교시</span>
-                                </div>
-                            ))
-                        }
+            return (<Card key={classRoom}>
+                <div className="Card-Title">{classRoom}</div>
+                <div className="Card-Detail">
+                    {
+                        classRoomList[classRoom].map(info => {
+                            const startTime = `${info.START < 1000 ? "0" : ""}${parseInt(info.START / 100)}:${info.START % 100}${info.START % 100 === 0 ? "0": ""}`;
+                            const endTime = `${info.END < 1000 ? "0" : ""}${parseInt(info.END / 100)}:${info.END % 100}${info.END % 100 === 0 ? "0": ""}`;
 
-                        <div className="flex flex-row justify-items-center my-2">
-                            <div className="Card-Detail-Stroke">
+                            return <div key={info.CLASSROOM_NM} className="flex flex-row justify-items-center my-2">
+                                <span className="mx-2">{`${info.SUBJECT_NM} (${info.PROFESSOR})`}</span>
+                                <span className="mx-2">{startTime} ~ {endTime}</span>
                             </div>
-                            <span className="mx-2">남았습니다.</span>
-                        </div>
-                    </div>
-                </Card>
-            );
+                        })
+                    }
+                </div>
+            </Card>);
         })}
     </AuthPage>
 }
