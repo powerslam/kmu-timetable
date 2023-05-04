8ebb7f0fb15a79749a6d8a51b63c1afdc5246f8d
timsort <timsort@kookmin.ac.kr>
2023-05-05 03:10:27 +0900

    TimeTable 안에 일별 보기를 TimeTablePage로 옮김

--- front/src/components/TimeTable/TimeTable.js
+++ front/src/components/TimeTable/TimeTable.js
@@ -6,33 +6,23 @@ import { WEEK_HEAD, TIME_HEAD, weekly, NULL_STR } from "../../lib/variables";
 import TimeTableCell from "./TimeTableCell";
 import TimeTableColumn from "./TimeTableColumn";
 
-import Card from '../common/Card';
 
-const TimeTable = ({ checked }) => {
-    return !checked ? 
-        <div className="div-table">
-            <div className="flex flex-row">
-                {WEEK_HEAD.map((v, i) => (
-                    <TimeTableCell key={i} text={v}/>
-                ))}
-            </div>
-                
-            <div className="flex flex-row">
-                <TimeTableColumn data={TIME_HEAD.alpha.daytime}/>
-                
-                <TimeTableColumn data={weekly[0].daytime} />
+const TimeTable = () => {
+    return <div className="div-table">
+        <div className="flex flex-row">
+            {WEEK_HEAD.map((v, i) => (
+                <TimeTableCell key={i} text={v}/>
+            ))}
+        </div>
+            
+        <div className="flex flex-row">
+            <TimeTableColumn data={TIME_HEAD.alpha.daytime}/>
+            
+            <TimeTableColumn data={weekly[0].daytime} />
 
-                <TimeTableColumn data={TIME_HEAD.number.daytime}/>
-            </div>
-        </div> : 
-        weekly[0].daytime.time.map((v, i) => {
-            if(v === NULL_STR) return null;
-            return (
-                <Card key={i}>
-                    <div></div>
-                </Card>
-            );
-        });
+            <TimeTableColumn data={TIME_HEAD.number.daytime}/>
+        </div>
+    </div>
 }
 
 export default TimeTable;
--- front/src/page/TimeTablePage.js
+++ front/src/page/TimeTablePage.js
@@ -1,17 +1,46 @@
 import { useState } from "react";
-import TimeTable from "../components/TimeTable/TimeTable";
 
+import Card from '../components/common/Card';
 import AuthPage from "../components/common/AuthPage";
+import TimeTable from "../components/TimeTable/TimeTable";
+import SearchMenu from "../components/SearchMenu/SearchMenu";
+
+import { weekly, NULL_STR } from "../lib/variables";
+import { MENU_OPEN, MENU_CLOSE, useServiceDispatch, useServiceState } from "../lib/ServiceContext";
 
 const TimeTablePage = () => {
     const [ checked, setChecked ] = useState(false);
     
+    const state = useServiceState();
+    const dispatch = useServiceDispatch();
+    
+    const getClass = () => {
+        dispatch({ type: !state.isMenuOpen ? MENU_OPEN : MENU_CLOSE });
+    }
+
     return <AuthPage>
-        <label className="m-4 text-2xl flex items-center justify-center w-2/5 max-lg:w-4/5">
-            일 단위로 보기&nbsp;
-            <input className="w-5 h-5" type="checkbox" checked={checked} onChange={() => setChecked(p => !p)} />
-        </label>
-        <TimeTable checked={checked} />        
+        <div className="flex flex-row justify-between w-full">
+            <label className="m-4 text-2xl flex items-center justify-center w-2/5 max-lg:w-4/5">
+                일 단위로 보기&nbsp;
+                <input className="w-5 h-5" type="checkbox" checked={checked} onChange={() => setChecked(p => !p)} />
+            </label>
+
+            <button className="btn btn-green" onClick={getClass}>과목 추가하기</button>
+        </div>
+        {!checked ? <TimeTable /> :
+            weekly[0].daytime.time.map((v, i) => {
+                if(v === NULL_STR) return null;
+                return (
+                    <Card key={i}>
+                        <div>{v}</div>
+                    </Card>
+                );
+            })
+        }
+
+        {state.isMenuOpen && (
+            <SearchMenu />
+        )}
     </AuthPage>
 };
 
