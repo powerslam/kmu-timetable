7f3f459fe74049af1ce75b4c2c4ee7360b3a785f
timsort <timsort@kookmin.ac.kr>
2023-05-06 02:13:18 +0900

    props 로 넘어온 데이터 노출

--- front/src/components/SearchMenu/SearchMenuItem.js
+++ front/src/components/SearchMenu/SearchMenuItem.js
@@ -1,6 +1,15 @@
-const SearchMenuItem = () => {
-    return <li className="Search-Menu-Item">
-        <div>안녕하세요?</div>
+import '../../styles/SearchMenu.css';
+
+const SearchMenuItem = ({ grade, sbjNM, sbjCD, division, credit, professor, time, week, remark}) => {
+    return <li className="MenuElement MenuItem">
+        <div className="Grade">{ grade }</div>
+        <div className="Subject">{`${sbjNM}(${sbjCD})`}</div>
+        <div className="Division">{ division }</div>
+        <div className="Credit">{ credit }</div>
+        <div className="Professor">{ professor }</div>
+        <div className="Time">{ time }</div>
+        <div className="Week">{ week }</div>
+        <div className="Remark">{ remark }</div>
     </li>
 };
 
