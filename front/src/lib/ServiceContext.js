875b005731447acf07e655311be599fee4c86c4d
timsort <timsort@kookmin.ac.kr>
2023-05-06 02:11:41 +0900

    속성명(userData -> payload) 변경 & 메뉴 관련 로직 추가

--- front/src/lib/ServiceContext.js
+++ front/src/lib/ServiceContext.js
@@ -1,36 +1,73 @@
 import { createContext, useContext, useReducer } from "react"
 
+export const SUBJECT_NM = "SUBJECT_NM";
+export const GRADE = "GRADE";
+export const WEEK = "WEEK";
+
 const initialState = {
     isLogin: !!localStorage.getItem('id'),
-    isMenuOpen: false,
+    menuData: null,
+
+    filterData: {
+        [SUBJECT_NM]: "",
+        [GRADE]: [],
+        [WEEK]: []
+    }
 };
 
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
 export const IS_LOGIN = "IS_LOGIN";
 
-export const MENU_OPEN = "MENU_OPEN";
-export const MENU_CLOSE = "MENU_CLOSE";
+export const UPDATE_MENU_ITEMS = "UPDATE_MENU_ITEMS";
+export const INPUT_SUBJECT_NAME = "INPUT_SUBJECT_NAME";
 
 const reducer = (state, action) => {
-    const { type, userData } = action;
+    const { type, payload } = action;
 
     switch(type) {
         case LOGIN: 
             localStorage.clear();
-            localStorage.setItem('id', userData.id);
-            localStorage.setItem('pwd', userData.pwd);
+            localStorage.setItem('id', payload.id);
+            localStorage.setItem('pwd', payload.pwd);
             return { isLogin: true };
                 
         case LOGOUT:
             localStorage.clear();
             return { isLogin: false };
 
-        case MENU_OPEN:
-            return { ...state, isMenuOpen: true };
-        
-        case MENU_CLOSE:
-            return { ...state, isMenuOpen: false };
+        case UPDATE_MENU_ITEMS:
+            return {
+                ...state,
+                menuData: payload,
+            };
+
+        case SUBJECT_NM:
+            return {
+                ...state,
+                filterData: {
+                    ...state.filterData,
+                    [SUBJECT_NM]: payload,
+                }
+            };
+
+        case GRADE:
+            return {
+                ...state,
+                filterData: {
+                    ...state.filterData,
+                    [GRADE]: payload,
+                }
+            };
+
+        case WEEK:
+            return {
+                ...state,
+                filterData: {
+                    ...state.filterData,
+                    [WEEK]: payload,
+                }
+            };
         
         default: return state;
     }
