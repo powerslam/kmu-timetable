cdfff42429d4a472ff73042620b4ec6f6f424e9e
powerslam <timsort@kookmin.ac.kr>
2023-05-09 01:36:10 +0900

    CSS 변경

--- front/src/page/TrafficPage.js
+++ front/src/page/TrafficPage.js
@@ -1,26 +1,23 @@
 import Card from "../components/common/Card";
 
-import { NULL_STR } from "../lib/variables";
+import "../styles/Card.css";
 
-// 아코디언
 const TrafficPage = () => {
     return (
-        <div className="page">
+        <div className="Page">
             {Array(4).fill(1).map((v, i) => {
-                if(v === NULL_STR) return null;
-                return (
-                    <Card key={i}>
-                        { /* 버스 이미지 / 지하철 이미지 넣기 */ }
-                        <div className="Card-Title">교통 수단 이름(ex 1711)</div>
-                        <div className="Card-Detail">
-                            { /* 슬라이더 같은거 활용해서 몇 정거장 남았는지 특히 지하철 같은 거 실시간 알림 */ }
-                            <div className="Card-Detail-Stroke">
-                                남은 시간
-                            </div>
-                            <span className="mx-2">남았습니다.</span>
+                return (<Card key={i}>
+                    <div className="Card-Title Card-Title-Text-Size">교통 수단 이름(ex 1711)</div>
+                    <div className="Card-Detail">
+                        { /* 슬라이더 같은거 활용해서 몇 정거장 남았는지 특히 지하철 같은 거 실시간 알림 */ }
+                        <div className="Card-Detail-Stroke Card-Detail-Margin">
+                            남은 시간
                         </div>
-                    </Card>
-                );
+                        <span style={{
+                            margin: "0.75rem"
+                        }}>남았습니다.</span>
+                    </div>
+                </Card>);
             })}
         </div>
     );
