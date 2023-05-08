abf78f5a001a27a2ef80fe8eb50a2bfd954d71e1
powerslam <timsort@kookmin.ac.kr>
2023-05-09 01:35:58 +0900

    CSS 변경 (Card)

--- front/src/components/common/Card.js
+++ front/src/components/common/Card.js
@@ -2,7 +2,7 @@ import "../../styles/Card.css";
 
 const Card = ({ children }) => {
     return (
-        <div className="Card">
+        <div className="Card Card-Size">
             { children }
         </div>
     );
