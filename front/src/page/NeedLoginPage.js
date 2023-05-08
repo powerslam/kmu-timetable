532f2e006b65854a97c5fa8435ed0e0f7e844aa1
powerslam <timsort@kookmin.ac.kr>
2023-05-09 01:34:32 +0900

    CSS 변경

--- front/src/page/NeedLoginPage.js
+++ front/src/page/NeedLoginPage.js
@@ -5,9 +5,16 @@ const NeedLoginPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <span>로그인이 필요한 서비스 입니다.</span>
-            <button onClick={() => navigate(LOGIN_PATH)}>로그인하러가기</button>
+        <div style={{textAlign: "center"}} className="Page">
+            <span style={{
+                fontSize: "1.5rem",
+                fontWeight: "600",
+                margin: "0.5rem",
+            }}>로그인이 필요한 서비스 입니다.</span>
+            <button className="btn bg-lightgreen sz-half" 
+                onClick={() => navigate(LOGIN_PATH)}>
+                    로그인하러가기
+            </button>
         </div>
     )
 };
